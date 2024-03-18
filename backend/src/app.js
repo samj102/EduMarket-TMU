8e61448d76329a66f158404e3f5c565fc8d97678
Divya Desai <divya.j.desai@ryerson.ca>
2024-03-18 16:01:50 -0400

    Add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const createError = require("http-errors");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const { authRouter } = require("./routes/auth");
 const connectDB = require("../config/db");
@@ -10,6 +11,7 @@ const app = express();
 
 require("dotenv").config();
 
+app.use(cors());
 app.use(logger("dev")); //logs requests
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
