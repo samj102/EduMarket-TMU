43c635a9682e01d8ddb6a23e94dcf7a6690159dd
NeelFaganiya12 <148932945+NeelFaganiya12@users.noreply.github.com>
2024-03-26 02:12:02 -0400

    implement bug fix for cookie auth

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -5,6 +5,7 @@ import { useNavigate, Link } from "react-router-dom";
 function Login({ setIsLoggedIn }) {
   setIsLoggedIn("false");
   const history = useNavigate();
+  document.body.style = "background: #B3FFCC";
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -23,6 +24,8 @@ function Login({ setIsLoggedIn }) {
             // console.log(res.data);
             setIsLoggedIn("true");
             localStorage.setItem("login", res.data.token);
+            document.cookie = `access_token=${res.data.token}`;
+
             history("/home", { state: { id: email, token: res.data.token } });
           } else if (res.status == 401) {
             alert("User has not sign up");
--- frontend/src/components/Users.js
+++ frontend/src/components/Users.js
@@ -1,44 +1,86 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, userNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
+import axios from "axios";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import api from "../utils/api";
+
+// function deleteUser(id, name) {
+//   if (window.confirm(`Are you sure you want to delete ${name}`)) {
+//     fetch(`http://localhost:8080/user/delete/${id}`, {
+//       method: "DELETE",
+//     })
+//       .then((res) => res.json())
+//       .then((data) => {
+//         console.log(data.data);
+//       });
+//   } else {
+//   }
+// }
 
 function Users() {
+  // document.body.backgroundColor = "#6AD8B1";
+  document.body.style = "background: #B3FFCC";
   const location = useLocation();
   const [data, setData] = useState([]);
 
-  function deleteUser(id, name) {
+  async function deleteUser(id, name) {
     if (window.confirm(`Are you sure you want to delete ${name}`)) {
-      fetch(`http://localhost:8080/user/delete/${id}`, {
-        method: "DELETE",
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          fetch("http://localhost:8080/user")
-            .then((res) => res.json())
-            .then((val) => setData(val));
-        });
+      await api.delete(`user/delete/${id}`);
+      const { data } = await api.get("/user");
+      setData(data);
     } else {
     }
   }
 
   //   Fetching users from the backend server
   useEffect(() => {
-    fetch("http://localhost:8080/user")
-      .then((res) => res.json())
-      .then((data) => setData(data));
+    api.get("/user").then(({ data }) => setData(data));
   }, []);
   try {
     const tok = localStorage.getItem("login");
+    console.log(tok);
     const decoded = jwtDecode(tok);
+    console.log(data);
     console.log(decoded);
     if (decoded.role == "admin") {
       return (
-        <div className="homepage">
+        <>
           <h1>List of Users currently signed up:</h1>
+          <div
+            className="homepage"
+            style={{ display: "flex", alignItems: "center" }}
+          >
+            <table
+              style={{
+                width: 500,
+                border: "1px solid",
+                backgroundColor: "white",
+              }}
+            >
+              <tr style={{ textAlign: "center", border: "1px solid" }}>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Role</th>
+                <th>Delete</th>
+              </tr>
+              {data.map((dt) => (
+                <tr style={{ textAlign: "center", border: "1px solid" }}>
+                  <td>{dt.name}</td>
+                  <td>{dt.email}</td>
+                  <td>{dt.role}</td>
+                  <td>
+                    <FontAwesomeIcon
+                      icon={faTrash}
+                      onClick={() => deleteUser(dt._id, dt.name)}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </table>
 
-          {data.map((dt) => (
+            {/* {data.map((dt) => (
             <div>
               <h1>
                 {dt.name}{" "}
@@ -48,9 +90,12 @@ function Users() {
                 />
               </h1>
               <h3>{dt.role}</h3>
+              <div>
+              </div>
             </div>
-          ))}
-        </div>
+          ))} */}
+          </div>
+        </>
       );
     } else {
       return (
