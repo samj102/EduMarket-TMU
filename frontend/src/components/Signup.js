ae01b0ab4c1a5c4b995b202aa826fd6d777935c5
NeelFaganiya12 <148932945+NeelFaganiya12@users.noreply.github.com>
2024-03-27 15:18:45 -0400

    beautified signup page

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -5,7 +5,6 @@ import { useNavigate, Link } from "react-router-dom";
 function Signup({ setIsLoggedIn }) {
   const history = useNavigate();
   setIsLoggedIn("false");
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
@@ -20,13 +19,6 @@ function Signup({ setIsLoggedIn }) {
           email: email,
           password: password,
         })
-        // .then((res) => {
-        //   if (res.data == "exist") {
-        //     alert("User already exists");
-        //   } else if (res.data == "notexist") {
-        //     history("/home", { state: { id: email } });
-        //   }
-        // })
         .then((res) => {
           if (res.status == 200) {
             // console.log(res.data);
@@ -51,40 +43,47 @@ function Signup({ setIsLoggedIn }) {
   }
 
   return (
-    <div className="login">
-      <h1>Signup</h1>
+    <div className="login_container">
+      <form action="POST" className="Form">
+        <h1>Sign Up</h1>
+        <div className="input_signup">
+          <input
+            type="name"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            placeholder="Name"
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            required
+          />
+        </div>
+        <div className="input_signup">
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            required
+          />
+        </div>
 
-      <form action="POST">
-        <input
-          type="name"
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-          placeholder="Name"
-        />
+        <button type="submit" onClick={submit}>
+          Resgister
+        </button>
 
-        <input
-          type="email"
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-          placeholder="Email"
-        />
-        <input
-          type="password"
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-          placeholder="Password"
-        />
-        <input type="submit" onClick={submit} />
+        <div className="register">
+          <p>
+            Already have an account? <Link to="/login">Login</Link>
+          </p>
+        </div>
       </form>
-
-      <br />
-      <p>OR</p>
-      <br />
-
-      <Link to="/login">Login Page</Link>
     </div>
   );
 }
