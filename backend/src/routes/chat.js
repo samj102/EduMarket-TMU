78f3908aa8178ca220f4f210f4253af4a363c330
Divya Desai <divya.j.desai@ryerson.ca>
2024-04-04 00:17:30 -0400

    Add route to get chat between two users

--- backend/src/routes/chat.js
+++ backend/src/routes/chat.js
@@ -39,6 +39,21 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
+router.get("/:user_id1/:user_id2", async (req, res, next) => {
+  const { user_id1, user_id2 } = req.params;
+  try {
+    const chat = await chatModel.find({
+      $or: [
+        { user_a: user_id1, user_b: user_id2 },
+        { user_a: user_id2, user_b: user_id1 },
+      ],
+    });
+    res.status(200).json(chat);
+  } catch (e) {
+    next(createError(500, e.message));
+  }
+});
+
 router.put("/:id", async (req, res, next) => {
   const { id } = req.params;
   const { sender, message } = req.body;
